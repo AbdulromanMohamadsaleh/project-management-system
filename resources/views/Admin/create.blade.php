f0033ce58a225af8d39daaff5a3e06e7471b7049
Abdulroman Mohamadsaleh <abdulromanmohamadsaleh2070@gmail.com>
2022-12-16 12:06:11 +0700

    form

--- resources/views/Admin/create.blade.php
+++ resources/views/Admin/create.blade.php
@@ -2,7 +2,7 @@
 @include('include.header')
 
 <body>
-    body>
+
     {{-- <div class="container-xxl position-relative bg-white d-flex p-0">
         <!-- Spinner Start -->
         <div id="spinner"
@@ -21,6 +21,7 @@
 
     <!-- Content Start -->
     <div class="content">
+
         <!-- Navbar Start -->
         <nav class="navbar navbar-expand bg-light navbar-light sticky-top px-4 py-0">
             <a href="index.html" class="navbar-brand d-flex d-lg-none me-4">
@@ -113,107 +114,89 @@
                 </div>
             </div>
         </nav>
+
         <!-- Recent Sales Start -->
         <div class="container-fluid">
             <div class="row justify-content-center">
-                <div class="col-11 col-sm-10 col-md-10 col-lg-6 col-xl-5 text-center p-0 mt-3 mb-2">
-                    <div class="card px-0 pt-4 pb-0 mt-3 mb-3">
-                        <h2 id="heading">Sign Up Your User Account</h2>
-                        <p>Fill all form field to go to next step</p>
-                        <form id="msform">
-                            <!-- progressbar -->
-                            <ul id="progressbar">
-                                <li class="active" id="account"><strong>Account</strong></li>
-                                <li id="personal"><strong>Personal</strong></li>
-                                <li id="payment"><strong>Image</strong></li>
-                                <li id="confirm"><strong>Finish</strong></li>
-                            </ul>
-                            <div class="progress">
-                                <div class="progress-bar progress-bar-striped progress-bar-animated"
-                                    role="progressbar" aria-valuemin="0" aria-valuemax="100"></div>
-                            </div> <br> <!-- fieldsets -->
-                            <fieldset>
-                                <div class="form-card">
-                                    <div class="row">
-                                        <div class="col-7">
-                                            <h2 class="fs-title">Account Information:</h2>
-                                        </div>
-                                        <div class="col-5">
-                                            <h2 class="steps">Step 1 - 4</h2>
-                                        </div>
-                                    </div> <label class="fieldlabels">Email: *</label> <input type="email"
-                                        name="email" placeholder="Email Id" /> <label class="fieldlabels">Username:
-                                        *</label> <input type="text" name="uname" placeholder="UserName" />
-                                    <label class="fieldlabels">Password: *</label> <input type="password"
-                                        name="pwd" placeholder="Password" /> <label class="fieldlabels">Confirm
-                                        Password: *</label> <input type="password" name="cpwd"
-                                        placeholder="Confirm Password" />
-                                </div> <input type="button" name="next" class="next action-button"
-                                    value="Next" />
-                            </fieldset>
-                            <fieldset>
-                                <div class="form-card">
-                                    <div class="row">
-                                        <div class="col-7">
-                                            <h2 class="fs-title">Personal Information:</h2>
-                                        </div>
-                                        <div class="col-5">
-                                            <h2 class="steps">Step 2 - 4</h2>
-                                        </div>
-                                    </div> <label class="fieldlabels">First Name: *</label> <input type="text"
-                                        name="fname" placeholder="First Name" /> <label class="fieldlabels">Last
-                                        Name: *</label> <input type="text" name="lname"
-                                        placeholder="Last Name" /> <label class="fieldlabels">Contact No.: *</label>
-                                    <input type="text" name="phno" placeholder="Contact No." /> <label
-                                        class="fieldlabels">Alternate Contact No.: *</label> <input type="text"
-                                        name="phno_2" placeholder="Alternate Contact No." />
-                                </div> <input type="button" name="next" class="next action-button"
-                                    value="Next" /> <input type="button" name="previous"
-                                    class="previous action-button-previous" value="Previous" />
-                            </fieldset>
-                            <fieldset>
-                                <div class="form-card">
-                                    <div class="row">
-                                        <div class="col-7">
-                                            <h2 class="fs-title">Image Upload:</h2>
-                                        </div>
-                                        <div class="col-5">
-                                            <h2 class="steps">Step 3 - 4</h2>
-                                        </div>
-                                    </div> <label class="fieldlabels">Upload Your Photo:</label> <input type="file"
-                                        name="pic" accept="image/*"> <label class="fieldlabels">Upload Signature
-                                        Photo:</label> <input type="file" name="pic" accept="image/*">
-                                </div> <input type="button" name="next" class="next action-button"
-                                    value="Submit" /> <input type="button" name="previous"
-                                    class="previous action-button-previous" value="Previous" />
-                            </fieldset>
-                            <fieldset>
-                                <div class="form-card">
-                                    <div class="row">
-                                        <div class="col-7">
-                                            <h2 class="fs-title">Finish:</h2>
-                                        </div>
-                                        <div class="col-5">
-                                            <h2 class="steps">Step 4 - 4</h2>
-                                        </div>
-                                    </div> <br><br>
-                                    <h2 class="purple-text text-center"><strong>SUCCESS !</strong></h2> <br>
-                                    <div class="row justify-content-center">
-                                        <div class="col-3"> <img src="https://i.imgur.com/GwStPmg.png"
-                                                class="fit-image"> </div>
-                                    </div> <br><br>
-                                    <div class="row justify-content-center">
-                                        <div class="col-7 text-center">
-                                            <h5 class="purple-text text-center">You Have Successfully Signed Up</h5>
-                                        </div>
-                                    </div>
+                {{-- <div class="col-11 col-sm-10 col-md-10 col-lg-6 col-xl-10 text-center p-0 mt-3 mb-2"> --}}
+                <div class="col-10 text-center p-0 mt-3 mb-2">
+
+                    <div style="border: none;" class="card px-0 pt-4 pb-0 mt-3 mb-3">
+
+                        <h1 class="text-center fs-4">Create Project</h1>
+                        <form id="signUpForm" action="#!">
+                            <!-- start step indicators -->
+                            <div class="form-header d-flex mb-4">
+                                <span class="stepIndicator">Detail</span>
+                                <span class="stepIndicator">Activity</span>
+                                {{-- <span class="stepIndicator">Personal Details</span> --}}
+                            </div>
+                            <!-- end step indicators -->
+
+                            <!-- step one -->
+                            <div class="step">
+                                <p class="text-center mb-4">Create your account</p>
+                                <div class="mb-3">
+                                    <input type="email" placeholder="Email Address" oninput="this.className = ''"
+                                        name="email">
                                 </div>
-                            </fieldset>
+                                <div class="mb-3">
+                                    <input type="password" placeholder="Password" oninput="this.className = ''"
+                                        name="password">
+                                </div>
+                                <div class="mb-3">
+                                    <input type="password" placeholder="Confirm Password"
+                                        oninput="this.className = ''" name="password">
+                                </div>
+                            </div>
+
+                            <!-- step two -->
+                            <div class="step">
+                                <p class="text-center mb-4">Your presence on the social network</p>
+                                <div class="mb-3">
+                                    <input type="text" placeholder="Linked In" oninput="this.className = ''"
+                                        name="linkedin">
+                                </div>
+                                <div class="mb-3">
+                                    <input type="text" placeholder="Twitter" oninput="this.className = ''"
+                                        name="twitter">
+                                </div>
+                                <div class="mb-3">
+                                    <input type="text" placeholder="Facebook" oninput="this.className = ''"
+                                        name="facebook">
+                                </div>
+                            </div>
+
+                            <!-- step three -->
+                            {{-- <div class="step">
+                                <p class="text-center mb-4">We will never sell it</p>
+                                <div class="mb-3">
+                                    <input type="text" placeholder="Full name" oninput="this.className = ''"
+                                        name="fullname">
+                                </div>
+                                <div class="mb-3">
+                                    <input type="text" placeholder="Mobile" oninput="this.className = ''"
+                                        name="mobile">
+                                </div>
+                                <div class="mb-3">
+                                    <input type="text" placeholder="Address" oninput="this.className = ''"
+                                        name="address">
+                                </div>
+                            </div> --}}
+
+                            <!-- start previous / next buttons -->
+                            <div class="form-footer d-flex">
+                                <button type="button" id="prevBtn" onclick="nextPrev(-1)">Previous</button>
+                                <button type="button" id="nextBtn" onclick="nextPrev(1)">Next</button>
+                            </div>
+                            <!-- end previous / next buttons -->
                         </form>
                     </div>
                 </div>
             </div>
             <!-- Recent Sales End -->
+
+
 
 
 </body>
