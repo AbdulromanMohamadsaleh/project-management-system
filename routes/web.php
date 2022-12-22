b3d3be4edf1ab788d227f8e9323fe9735d9af915
Abdulroman Mohamadsaleh <abdulromanmohamadsaleh2070@gmail.com>
2022-12-22 08:54:55 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -23,9 +23,8 @@ Route::get('/', function () {
 Route::get('/login', [CustomLoginController::class, 'login'])->name('login');
 
 Route::get('/admin', [ProjectController::class, 'Index']);
-Route::get('/table', [ProjectController::class,'Table'])->name('table');
-Route::get('/create', [ProjectController::class,'Create'])->name('create');
-Route::get('/show/{id}', [ProjectController::class,'show'])->name('show');
-Route::get('/approve/{id}', [ProjectController::class,'Approve'])->name('approve');
-
-
+Route::get('/table', [ProjectController::class, 'Table'])->name('table');
+Route::get('/create', [ProjectController::class, 'Create'])->name('create');
+Route::get('/show/{id}', [ProjectController::class, 'show'])->name('show');
+Route::get('/approve/{id}', [ProjectController::class, 'Approve'])->name('approve');
+Route::get('/done/{id}', [ProjectController::class, 'Done'])->name('done');
