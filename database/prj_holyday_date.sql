6e361a6ca0599871fc06a37b4c2c14ce016b222b
arifinsotaloh <120566126+arifinsotaloh@users.noreply.github.com>
2022-12-23 16:03:01 +0700

    holyday.

---
+++ database/prj_holyday_date.sql
@@ -0,0 +1,66 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 23, 2022 at 10:02 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.0.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `prj_project`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `prj_holyday_date`
+--
+
+CREATE TABLE `prj_holyday_date` (
+  `HOLYDAY_ID` varchar(20) NOT NULL,
+  `HOLYDAY_NAME` varchar(50) NOT NULL,
+  `HOLYDAY_DATE` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `prj_holyday_date`
+--
+
+INSERT INTO `prj_holyday_date` (`HOLYDAY_ID`, `HOLYDAY_NAME`, `HOLYDAY_DATE`) VALUES
+('01', 'New Year Holiday', '2023-01-02'),
+('02', 'Chakri Day', '2023-04-06'),
+('03', 'Labour Day ', '2023-05-01'),
+('04', 'Coronation of King V', '2023-05-04'),
+('05', 'Queen Suthida\'s Birthday Holiday', '2023-06-05'),
+('06', 'King Vajiralongkorn\'s Birthday', '2023-06-28'),
+('07', ' Mother\'s Birthday Holiday', '2023-08-14'),
+('08', 'Passing of His Majesty the Late King', '2023-10-13'),
+('09', 'Chulalongkorn Memorial Day', '2023-10-23'),
+('10', 'His Majesty the Late King\'s Birthday', '2023-12-05'),
+('11', 'Constitution Day Holiday', '2023-10-11');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `prj_holyday_date`
+--
+ALTER TABLE `prj_holyday_date`
+  ADD PRIMARY KEY (`HOLYDAY_ID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
