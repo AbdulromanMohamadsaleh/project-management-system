d58f12172ad078790d8d3a3c2f05674e8b91a821
Abdulroman Mohamadsaleh <abdulromanmohamadsaleh2070@gmail.com>
2022-12-28 16:05:13 +0700

    fix task

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\ProjectTask;
 use Illuminate\Http\Request;
+use App\Models\ProjectDetial;
 use App\Models\ProjectActivity;
 
 class TaskController extends Controller
@@ -17,6 +18,12 @@ class TaskController extends Controller
 
         $completeTasksCounter = ProjectTask::where('ACTIVITY_ID', $Task->activity->ACTIVITY_ID)->where('STATUS', 1)->get();
         $totalTasks = ProjectTask::where('ACTIVITY_ID', $Task->activity->ACTIVITY_ID)->count();
+
+        if ($completeTasksCounter->count() - 1 == 0) {
+            $Project = ProjectDetial::where('DETAIL_ID', $Task->activity->DETAIL_ID)->first();
+            $Project->STATUS = "New Release,Approved,Progress,workingOn";
+            $Project->save();
+        }
 
         if ($completeTasksCounter->count() == $totalTasks) {
             $Task->activity->STATUS = 1;
