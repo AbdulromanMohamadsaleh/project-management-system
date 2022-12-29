882fa273891294c21171da2119c69567a7c66a9d
arifinsotaloh <120566126+arifinsotaloh@users.noreply.github.com>
2022-12-29 15:54:50 +0700

    script

--- resources/views/include/scrip.blade.php
+++ resources/views/include/scrip.blade.php
@@ -17,3 +17,4 @@
     integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous">
 </script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
