1f9da944b55c03baf0a8224203b5733cacc98ee4
Abdulroman Mohamadsaleh <abdulromanmohamadsaleh2070@gmail.com>
2023-01-09 09:53:02 +0700

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -9,6 +9,12 @@ use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
+
+    public function index()
+    {
+        return view('Admin.index');
+    }
+
     public function handle(Request $request, Closure $next)
     {
         if (Auth::check() && Auth::user()->POSITION == 'Admin') {
