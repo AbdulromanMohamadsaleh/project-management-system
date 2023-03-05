abde5a33a1215665d92de981692e7d8a0f448443
Abdulroman Mohamadsaleh <abdulromanmohamadsaleh2070@gmail.com>
2023-03-05 21:41:56 +0700

    create Models

---
+++ app/Models/Privilege.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Privilege extends Model
+{
+    use HasFactory;
+
+    protected $table = 'prj_privilege';
+    protected $primaryKey = 'PRIV_ID';
+    protected $keyType = 'string';
+}
